ab7f3c09142d2d8f1b632428d713243d98227a41
kent10ou <kent10ou@gmail.com>
2015-10-08 15:15:16 -0700

    fibonacci

---
+++ nthFibonacci.js
@@ -0,0 +1,41 @@
+/**
+ * A Fibonacci sequence is a list of numbers that begins with 0 and 1, and each
+ * subsequent number is the sum of the previous two.
+ *
+ * For example, the first five Fibonacci numbers are:
+ *
+ *   0 1 1 2 3
+ *
+ * If n were 4, your function should return 3; for 5, it should return 5.
+ *
+ * Write a function that accepts a number, n, and returns the nth Fibonacci
+ * number. Use a recursive solution to this problem; if you finish with time
+ * left over, implement an iterative solution.
+ *
+ * example usage:
+ * nthFibonacci(2); // => 1
+ * nthFibonacci(3); // => 2
+ * nthFibonacci(4); // => 3
+ * etc...
+ *
+ */
+
+//Recursive solution
+ var nthFibonacci = function (n) {
+  if (n < 2) { return n; } 
+  else {
+    return nthFibonacci(n - 1) + nthFibonacci(n -2);
+  }
+};
+
+//Iterative solution
+var nthFibonacci = function (n) {
+  fibArr = [0, 1]
+  if (n < 2){ return n} 
+  else {
+    for (var i = 2; i <= n; i++) {
+      fibArr[i] = fibArr[i-1] + fibArr[i-2]   
+    }
+  }
+  return fibArr[n];
+}
