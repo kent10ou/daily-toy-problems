fcdb1827b145fb0138cdb3c11e8b96bcaaec6a01
kent10ou <kent10ou@gmail.com>
2015-10-09 17:09:10 -0700

    fixed swap

--- swapw:oTemp.js
+++ swapw:oTemp.js
@@ -5,9 +5,11 @@ var arr = [5,2,1,3,6];
 //swap 2nd and 5th index
 //results = [5,6,1,3,2];
 
-function (a, b, array) {
+function swap (a, b, array) {
 	array[a] = array[a] + array[b]
-	array[a] = array[a] - array[b]
+	array[b] = array[a] - array[b]
 	array[a] = array[a] - array[b]
 	return array
 }
+
+console.log(swap(1, 4, arr))
