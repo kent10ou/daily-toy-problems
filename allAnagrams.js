158cdbeaf00397fd0a8bb8865b168ab76a54a1f6
kent10ou <kent10ou@gmail.com>
2015-12-21 15:28:45 -0800

    recursive solution

--- allAnagrams.js
+++ allAnagrams.js
@@ -4,3 +4,17 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
+var allAnagrams = function (string) {
+	var results = {};
+	var recurse = function (perm, left) {
+		if (left === '') {
+			results[left] = true;
+		}
+		for (var i = 0; i < left.length; i++) {
+			recurse(perm + left[i], left.slice(0, i) + left.slice(i+1));
+		}
+	}
+	recurse('', string);
+
+	return Object.keys(results);
+}
