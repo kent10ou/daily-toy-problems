edd575c1cffd91d6e7c3956401d61bab46371ecd
kent10ou <kent10ou@gmail.com>
2015-12-21 15:40:03 -0800

    doesn't work yet

--- allAnagrams.js
+++ allAnagrams.js
@@ -6,15 +6,17 @@
 
 var allAnagrams = function (string) {
 	var results = {};
-	var recurse = function (perm, left) {
+	(function recurse (perm, left) {
 		if (left === '') {
 			results[left] = true;
 		}
 		for (var i = 0; i < left.length; i++) {
 			recurse(perm + left[i], left.slice(0, i) + left.slice(i+1));
 		}
-	}
-	recurse('', string);
+	})('', string);
 
 	return Object.keys(results);
 }
+
+var anagrams = allAnagrams('abc');
+console.log(anagrams);
